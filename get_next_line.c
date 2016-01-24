8b752d1cc87eba8225d72fe536bab761d6584c18
Arthur DU PELOUX <adu-pelo@e1r1p12.42.fr>
2016-01-24 16:24:08 +0100

    ret val corrected

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: adu-pelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/12 16:28:18 by adu-pelo          #+#    #+#             */
-/*   Updated: 2016/01/13 17:10:48 by adu-pelo         ###   ########.fr       */
+/*   Updated: 2016/01/24 16:22:49 by adu-pelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ static char *ft_mem_concat(char *str, char *buff)
 	char *tmp;
 
 	tmp = ft_strjoin(str, buff);
-	free(str);
-	str = NULL;
+	ft_strdel(&str);
 	return (tmp);
 }
 
@@ -39,15 +38,17 @@ static int	ft_get_line(char **line, char *buff, char *overf)
 
 int			get_next_line(int const fd, char **line)
 {
-	static char overf[BUFF_SIZE + 1];
+	static char overf[255][BUFF_SIZE + 1];
 	char		buff[BUFF_SIZE + 1];
 	char 		*str;
 	int 		ret;
 
-	ft_bzero(buff, BUFF_SIZE);
-	if (ft_get_line(line, overf, overf) == 1)
+	if (fd < 0 || line == NULL)
+		return (-1);
+	if (ft_get_line(line, overf[fd], overf[fd]) == 1)
 		return (1);
-	str = ft_strdup(overf);
+	str = ft_strdup(overf[fd]);
+	ft_memset(overf[fd], 0, BUFF_SIZE + 1);
 	ft_bzero(buff, BUFF_SIZE + 1);
 	while ((ret = read(fd, buff, BUFF_SIZE)) != 0)
 	{
@@ -55,37 +56,15 @@ int			get_next_line(int const fd, char **line)
 			return (-1);
 		buff[ret] = '\0';
 		str = ft_mem_concat(str, buff);
-		if (ft_get_line(line, str, overf) == 1)
+		if (ft_get_line(line, str, overf[fd]) == 1)
 		{
-			free(str);
-			str = NULL;
+			ft_strdel(&str);
 			return (1);
 		}
 	}
-	free(str);
-	str = NULL;
-	*line = NULL;
-	return (0);
-}
-
-int		main(int ac, char **av)
-{
-	int fd;
-	int i;
-	char *line;
-
-	line = NULL;
-	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
-		return (-1);
-	i = 1;
-	while (i == 1)
-	{
-		i = get_next_line(fd, &line);
-		printf("%d || %s\n", i, line);
-		free(line);
-		line = NULL;
-	}
-	close(fd);
-	return (0);
+	return ((*line = str) && ft_strlen(*line) != 0);
+	//free(str);
+	//str = NULL;
+	//*line = NULL;
+	//return (0);
 }
---
+++ main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adu-pelo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/24 16:21:34 by adu-pelo          #+#    #+#             */
+/*   Updated: 2016/01/24 16:21:58 by adu-pelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int		main(int ac, char **av)
+{
+	int		fd;
+	int		ret_value;
+	char	*line;
+
+	line = NULL;
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	ret_value = 1;
+	while (ret_value == 1)
+	{
+		ret_value = get_next_line(fd, &line);
+		printf("%d || %s\n", ret_value, line);
+	}
+	close(fd);
+	return (0);
+}
